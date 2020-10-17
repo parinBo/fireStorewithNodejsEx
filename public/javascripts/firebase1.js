29e9f1a5e6c24582b309c537760f88a78997a309
parin <bpyr.wk@gmail.com>
2020-10-18 03:25:26 +0700

    finish regis page and doing login

--- public/javascripts/firebase1.js
+++ public/javascripts/firebase1.js
@@ -1,30 +1,34 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyD0pmOFpY1iga1FPXVhJLPBx1U2bAhGcbI",
-    authDomain: "my-awesome-ed3bc.firebaseapp.com",
-    databaseURL: "https://my-awesome-ed3bc.firebaseio.com",
-    projectId: "my-awesome-ed3bc",
-    storageBucket: "my-awesome-ed3bc.appspot.com",
-    messagingSenderId: "32965048341",
-    appId: "1:32965048341:web:432c3462e254e6a642761c",
-    measurementId: "G-2YN72KKECF"
+    apiKey: "AIzaSyDQiIe2lO-woUfXfas6nYc_WFZrPAdX6vw",
+    authDomain: "fund-662cf.firebaseapp.com",
+    databaseURL: "https://fund-662cf.firebaseio.com",
+    projectId: "fund-662cf",
+    storageBucket: "fund-662cf.appspot.com",
+    messagingSenderId: "159155807852",
+    appId: "1:159155807852:web:0a6c1ab9c235814c401a49",
+    measurementId: "G-E4RHCLCLVX"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 var usersdb = db.collection("users")
 const form = document.querySelector("#addform")
-
+document.getElementsByClassName("spinner")[0].hidden = true
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    usersdb.add({
-        username: form.username.value,
-        password: form.password.value
-    })
-    var spinner = document.createElement("div")
-    spinner.setAttribute("class", "spinner-border")
-    spinner.setAttribute("style", "width: 3rem; height: 3rem;");
-    document.getElementById("submit").disabled = true;
-    $("#submit").hide()
-    document.getElementById("spinner").appendChild(spinner)
-
-
+    var findit = usersdb.where("username", "==", document.getElementById("username").value).get()
+    findit.then(querySnapshot => {
+        if (!querySnapshot.empty) {
+            alert("username is used!")
+        } else {
+            usersdb.add({
+                username: form.username.value,
+                password: form.password.value
+            })
+            form.hidden = true;
+            document.getElementsByClassName("spinner")[0].hidden = false
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 400)
+        }
+    });
 })
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const admin = require('firebase-admin');
+const serviceAccount = require('./fund-662cf-firebase-adminsdk-myvrb-6b8553112d.json');
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/data', function(req, res, next) {
-    res.render('adddata', { title: 'Express' });
+router.get('/regis', function(req, res, next) {
+    res.render('regis', { title: "Login", style: "<link rel=stylesheet href=/>" });
+});
+
+
+
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: "Login", style: "" });
+});
+router.post('/auth/login', function(req, res, next) {
+    const { username, password } = req.body
+    const users = db.collection('users');
+    const snapshot = users.where('username', '==', username).get();
+    snapshot.then((e) => {
+        e.forEach(element => {
+            console.log(element.data())
+        });
+    })
+    res.render('login', { title: "username or password wrong!", style: "<link rel=stylesheet href=/stylesheets/normal.css>" });
 });
 module.exports = router;
